85bb59e5d4f9ede9b816901e6593216fca23569f
hualr <hualefeng@outlook.com>
2022-05-01 14:09:48 +0800

    可视性

--- springboot-kafka-demo/src/main/java/com/cheng/kafka/com/consumer/KafkaConsumer.java
+++ springboot-kafka-demo/src/main/java/com/cheng/kafka/com/consumer/KafkaConsumer.java
@@ -1,18 +1,35 @@
 package com.cheng.kafka.com.consumer;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaConsumer {
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
 
 
-    @KafkaListener(topics = "myTopic",groupId = "defaultConsumerGroup")
-    public void listenDefaultGroup(ConsumerRecord<String, String> record, Acknowledgment ack) {
+    @KafkaListener(topics = "topic1", groupId = "defaultConsumerGroup1")
+    public void listenDefaultGroup1(ConsumerRecord<String, String> record, Acknowledgment ack) {
         String value = record.value();
-        System.out.println("接受消息如下：======================================start========================");
+        System.out.println("接消费阶段1消息如下：======================================start========================");
+        System.out.println(value);
+        System.out.println(record);
+        System.out.println("================================end==============================");
+        //手动提交offset
+        ack.acknowledge();
+
+        kafkaTemplate.send("topic2", "msg:" + "收到消息后发送");
+    }
+
+    @KafkaListener(topics = "topic2", groupId = "defaultConsumerGroup2")
+    public void listenDefaultGroup2(ConsumerRecord<String, String> record, Acknowledgment ack) {
+        String value = record.value();
+        System.out.println("消费阶段2：======================================start========================");
         System.out.println(value);
         System.out.println(record);
         System.out.println("================================end==============================");
--- springboot-kafka-demo/src/main/java/com/cheng/kafka/com/controller/KafkaController.java
+++ springboot-kafka-demo/src/main/java/com/cheng/kafka/com/controller/KafkaController.java
@@ -1,25 +1,24 @@
 package com.cheng.kafka.com.controller;
 
+import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
-
 @RestController
 @RequestMapping("/rest")
 public class KafkaController {
 
-    private final static String TOPIC_NAME = "myTopic";
+    private final static String TOPIC_NAME = "topic1";
 
     @Autowired
-    private KafkaTemplate<String,String> kafkaTemplate;
+    private KafkaTemplate<String, String> kafkaTemplate;
 
     @RequestMapping("/send")
-    public void send(){
+    public void send() {
         Date now = new Date();
-        System.out.println("发送时间：=================  "+now.toString());
-        kafkaTemplate.send(TOPIC_NAME,"msg:"+now.toString());
+        System.out.println("发送时间：=================  " + now);
+        kafkaTemplate.send(TOPIC_NAME, "msg:" + "发送消息" + now);
     }
 }
